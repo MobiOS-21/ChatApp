0f1d1622b3640a74accb264e4acabe0033b5601d
d <aleksandrdergelev@mail.ru>
2021-09-20 21:56:44 +0300

    Initial Commit

---
+++ chat-app-MobiOS-21/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  chat-app-MobiOS-21
+//
+//  Created by Александр Дергилёв on 20.09.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ chat-app-MobiOS-21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chat-app-MobiOS-21
+//
+//  Created by Александр Дергилёв on 20.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
