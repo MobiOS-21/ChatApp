6bb12b8b1f6aa1ed3bfc7c1f6b31e0de0a3887bf
d <aleksandrdergelev@mail.ru>
2021-09-21 22:16:31 +0300

    added new scheme without print, added icon

--- chat-app-MobiOS-21/AppDelegate.swift
+++ chat-app-MobiOS-21/AppDelegate.swift
@@ -9,11 +9,37 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        debugPrint("application moved from not running to inactive: \(#function)")
         return true
+    }
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        debugPrint("application will move from not running to inactive: \(#function)")
+        return true
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        debugPrint("application moved from inactive to active: \(#function)")
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        debugPrint("application moved from active to inactive: \(#function)")
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        debugPrint("application moved from inactive to background \(#function)")
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        debugPrint("application moved from background to active: \(#function)")
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        debugPrint("application moved from background to close \(#function)")
     }
 }
 
