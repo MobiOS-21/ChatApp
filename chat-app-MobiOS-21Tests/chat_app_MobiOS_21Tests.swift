ede27ddb20e481865b08c5d44e1b036626126a23
d <aleksandrdergelev@mail.ru>
2021-12-06 21:05:10 +0300

    maked ui tests

---
+++ chat-app-MobiOS-21Tests/chat_app_MobiOS_21Tests.swift
@@ -0,0 +1,32 @@
+//
+//  chat_app_MobiOS_21Tests.swift
+//  chat-app-MobiOS-21Tests
+//
+//  Created by Александр Дергилёв on 06.12.2021.
+//
+
+import XCTest
+
+class ChatAppMobiOS21Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
