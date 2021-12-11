8df835c7219dfb38f96f8c572e5b9e11e5665169
d <aleksandrdergelev@mail.ru>
2021-12-11 16:31:06 +0300

    maked unit tests for network service

---
+++ chat-app-MobiOS-21Tests/CoreMocks/Network/ImageRequestMock.swift
@@ -0,0 +1,13 @@
+//
+//  ImageRequestMock.swift
+//  chat-app-MobiOS-21Tests
+//
+//  Created by Александр Дергилёв on 11.12.2021.
+//
+
+import Foundation
+@testable import chat_app_MobiOS_21
+
+class ImageRequestMock: IRequest {
+    var urlRequest: URLRequest?
+}
