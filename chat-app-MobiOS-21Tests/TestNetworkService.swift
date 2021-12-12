ac8a1dfff845c98a3be2dac981fb088520bb8661
d <aleksandrdergelev@mail.ru>
2021-12-12 20:03:22 +0300

    fixed unit test and app settings

--- chat-app-MobiOS-21Tests/TestNetworkService.swift
+++ chat-app-MobiOS-21Tests/TestNetworkService.swift
@@ -2,66 +2,24 @@
 //  TestNetworkService.swift
 //  chat-app-MobiOS-21Tests
 //
-//  Created by Александр Дергилёв on 11.12.2021.
+//  Created by Александр Дергилёв on 12.12.2021.
 //
 
 import XCTest
 @testable import chat_app_MobiOS_21
 
 class TestNetworkService: XCTestCase {
-    func testBadURLRequest() {
-        let imageRequest = ImageRequestMock()
-        let imageItemParser = ImageItemParserMock(mockResponse: nil)
+    func testFailureNetworkRequest() {
         let requestSender = RequestSenderMock()
+        let concurencySender = ConcurrencySender()
+        let networkService = NetwrokService(requestSender: requestSender, concurencySender: concurencySender)
         
-        requestSender.send(config: RequestConfig(request: imageRequest, parser: imageItemParser)) { result in
-            switch result {
-            case .success:
-                XCTFail("Test error")
-            case .failure(let error):
-                XCTAssertEqual(NetworkError.badURL.errorDescription, error.localizedDescription)
-            }
-        }
-    }
-    
-    func testNoDataRequest() {
-        guard let url = URL(string: "www.test.com") else {
-            XCTFail("Bad URL")
-            return
-        }
-        let imageRequest = ImageRequestMock()
-        imageRequest.urlRequest = URLRequest(url: url)
-        let imageItemParser = ImageItemParserMock(mockResponse: nil)
-        let requestSender = RequestSenderMock()
-        
-        requestSender.send(config: RequestConfig(request: imageRequest, parser: imageItemParser)) { result in
+        networkService.fetchImages { result in
             switch result {
             case .success:
                 XCTFail("Test error")
             case .failure(let error):
                 XCTAssertEqual(NetworkError.noData.errorDescription, error.localizedDescription)
-            }
-        }
-    }
-    
-    func testCorrectRequest() {
-        guard let url = URL(string: "www.test.com") else {
-            XCTFail("Bad URL")
-            return
-        }
-        let mockResponse = ImagesResponse(hits: [Hit(webformatURL: "www.test.com")])
-        let imageRequest = ImageRequestMock()
-        imageRequest.urlRequest = URLRequest(url: url)
-        let imageItemParser = ImageItemParserMock(mockResponse: mockResponse)
-        let requestSender = RequestSenderMock()
-        
-        requestSender.send(config: RequestConfig(request: imageRequest, parser: imageItemParser)) { result in
-            switch result {
-            case .success(let response):
-                XCTAssertEqual(mockResponse.hits.count, response.hits.count)
-                XCTAssertEqual(mockResponse.hits.first?.webformatURL, response.hits.first?.webformatURL)
-            case .failure:
-                XCTFail("Test error")
             }
         }
     }
